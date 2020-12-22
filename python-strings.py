92b548681a0a587d486d6fdbd03c548a04bdccf0
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-12-22 15:06:43 +0600

    Update python-strings.py

--- python-strings.py
+++ python-strings.py
@@ -1,7 +1,6 @@
 '''Strings in Python are identified as a contiguous set of characters represented in the quotation marks.
 Python allows either pair of single or double quotes. Subsets of strings can be taken using the slice operator ([ ]
 and [:] ) with indexes starting at 0 in the beginning of the string and working their way from -1 to the end.
-
 The plus (+) sign is the string concatenation operator and the asterisk (*) is the repetition operator. For example −'''
 
 str = 'Hello World!'
