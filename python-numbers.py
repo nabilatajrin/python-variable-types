11ce2a8a5d1c1fe5f9f013518b77d6a09d4727b3
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2020-12-22 15:05:43 +0600

    Update python-numbers.py

--- python-numbers.py
+++ python-numbers.py
@@ -4,17 +4,13 @@ var1 = 1
 var2 = 10
 
 #You can also delete the reference to a number object by using the del statement. The syntax of the del statement is −
-
 #del var1[,var2[,var3[....,varN]]]]
-
 #You can delete a single object or multiple objects by using the del statement. For example −
 
 del var1
-
 print(var1)
 
 '''Python supports three different numerical types −
-
 int (signed integers)
 float (floating point real values)
 complex (complex numbers)
@@ -22,13 +18,8 @@ All integers in Python3 are represented as long integers. Hence, there is no sep
 
 '''Examples
 Here are some examples of numbers −
-
 int	float	complex
 10	0.10	3.14j'''
 
-
 '''A complex number consists of an ordered pair of real floating-point numbers denoted by x + yj, 
 where x and y are real numbers and j is the imaginary unit.'''
-
-
-
